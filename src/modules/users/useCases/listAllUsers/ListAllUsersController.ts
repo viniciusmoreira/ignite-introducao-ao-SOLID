3caffdae9e654e07a8520e2704a9cd7bc8cbe5f8
Vinicius Moreira <vinicius.moreira@godoo.com.br>
2021-03-22 17:59:05 -0300

    Ajuste no user_id do ListAllUsersController

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -2,20 +2,16 @@ import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
-interface IRequest extends Request {
-  headers: {
-    user_id: string;
-  };
-}
-
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: IRequest, response: Response): Response {
+  handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
 
     try {
-      const users = this.listAllUsersUseCase.execute({ user_id });
+      const users = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
 
       return response.json(users);
     } catch (error) {
